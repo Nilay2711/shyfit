6cab829cc0fae58e475f931a9e5d3ad442486afe
Nilay <nilay.pandya3@gmail.com>
2026-04-16 22:49:03 +0530

    -trial 1

--- backend/retriever.py
+++ backend/retriever.py
@@ -6,7 +6,9 @@ from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-with open('final_dataset.json',"r") as f:
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(BASE_DIR, "final_dataset.json"), "r") as f:
     data = json.load(f)
 
 documents = []
